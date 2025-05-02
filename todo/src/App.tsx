e15e9f9d43b0504b41746183e1c096a5816b102a
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-05-02 18:09:22 +0530

    feat: implement task management functionality with dynamic date display and delete option

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,4 +1,5 @@
 import { CirclePlus, Trash2 } from "lucide-react";
+import { ChangeEvent, useState } from "react";
 
 const App = () => {
   const randomColor = () => {
@@ -15,17 +16,50 @@ const App = () => {
     };
   };
 
+  const date = () => {
+    const today = new Date();
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    };
+    return today.toLocaleDateString("en-US", options); // Example: "May 2, 2025"
+  };
+
   const { bgColor, textColor } = randomColor();
+  const [task, setTask] = useState<{ task: string }>({ task: "" });
+  const [data, setData] = useState<{ task: string }[]>([]);
+
+  const handlerChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTask({ ...task, [e.target.name]: e.target.value });
+  };
+
+  const handlerClick = (
+    e: React.MouseEvent<HTMLButtonElement | HTMLDivElement>
+  ) => {
+    e.preventDefault();
+    if (task.task.trim() !== "") {
+      setData([...data, { ...task }]);
+      setTask({ task: "" });
+    }
+  };
+
+  const handlerDelete = (idx: number) => {
+    setData(data.filter((_, id) => id != idx));
+  };
 
   return (
     <>
       <div className="flex flex-col gap-5 h-screen w-screen p-20 font-[Helvetica_Now_Display]">
         <div className="text-4xl tracking-tighter">To-Do</div>
         <div className="bg-gray-300 w-full h-0.5 rounded-full"></div>
-        <div className="flex items-center gap-5">
+        <div onClick={handlerClick} className="flex items-center gap-5">
           <input
             type="text"
             placeholder="Add a new task..."
+            name="task"
+            value={task.task}
+            onChange={handlerChange}
             className="border-b-2 border-gray-300 outline-none "
           />
           <button className="flex gap-2 px-5 py-2 bg-blue-600 text-zinc-100 font-semibold rounded-md max-w-fit cursor-pointer active:scale-95 hover:bg-blue-500">
@@ -33,61 +67,69 @@ const App = () => {
           </button>
         </div>
         <div className="flex flex-col gap-3">
-          <div className="border border-gray-300 rounded-md px-5 py-2 shadow-2xs">
-            <div className="flex gap-4 justify-between">
-              <div className="flex items-start gap-4">
-                <input
-                  type="checkbox"
-                  name=""
-                  id=""
-                  className="mt-1.5 h-5 w-5 text-green-600 focus:ring-green-500"
-                />
-                <div className="flex flex-col gap-1.5">
-                  <div className="font-bold text-lg">Cricket</div>
-                  <div className="text-gray-500 font-semibold text-sm">
-                    📅 Jan 8, 2024
-                  </div>
-                  <div className="flex items-center gap-4">
-                    <div
-                      style={{
-                        backgroundColor: bgColor,
-                        color: textColor,
-                      }}
-                      className="text-sm px-5 py-1 rounded-full flex items-center justify-center gap-2"
-                    >
-                      <span
-                        className="h-2 w-2 rounded-full"
-                        style={{
-                          backgroundColor: textColor,
-                          boxShadow: `0 0 8px ${textColor}, 0 0 12px ${textColor}`,
-                        }}
-                      ></span>
-                      Project
+          {data.map((item, idx) => (
+            <div
+              key={idx}
+              className="border border-gray-300 rounded-md px-5 py-2 shadow-2xs"
+            >
+              <div className="flex gap-4 justify-between">
+                <div className="flex items-start gap-4">
+                  <input
+                    type="checkbox"
+                    name=""
+                    id=""
+                    className="mt-1.5 h-5 w-5 text-green-600 focus:ring-green-500"
+                  />
+                  <div className="flex flex-col gap-1.5">
+                    <div className="font-bold text-lg">{item.task}</div>
+                    <div className="text-gray-500 font-semibold text-sm">
+                      📅 {date()}
                     </div>
-                    <div
-                      style={{
-                        backgroundColor: bgColor,
-                        color: textColor,
-                      }}
-                      className="text-sm px-5 py-1 rounded-full flex items-center justify-center gap-2"
-                    >
-                      <span
-                        className="h-2 w-2 rounded-full"
+                    <div className="flex items-center gap-4">
+                      <div
                         style={{
-                          backgroundColor: textColor,
-                          boxShadow: `0 0 8px ${textColor}, 0 0 12px ${textColor}`,
+                          backgroundColor: bgColor,
+                          color: textColor,
                         }}
-                      ></span>
-                      Project
+                        className="text-sm px-5 py-1 rounded-full flex items-center justify-center gap-2"
+                      >
+                        <span
+                          className="h-2 w-2 rounded-full"
+                          style={{
+                            backgroundColor: textColor,
+                            boxShadow: `0 0 8px ${textColor}, 0 0 12px ${textColor}`,
+                          }}
+                        ></span>
+                        Project
+                      </div>
+                      <div
+                        style={{
+                          backgroundColor: bgColor,
+                          color: textColor,
+                        }}
+                        className="text-sm px-5 py-1 rounded-full flex items-center justify-center gap-2"
+                      >
+                        <span
+                          className="h-2 w-2 rounded-full"
+                          style={{
+                            backgroundColor: textColor,
+                            boxShadow: `0 0 8px ${textColor}, 0 0 12px ${textColor}`,
+                          }}
+                        ></span>
+                        Project
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="hover:text-red-600 hover:rounded-full cursor-pointer active:scale-95 active:text-red-500">
-                <Trash2 />
+                <div
+                  onClick={() => handlerDelete(idx)}
+                  className="hover:text-red-600 hover:rounded-full cursor-pointer active:scale-95 active:text-red-500"
+                >
+                  <Trash2 />
+                </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
